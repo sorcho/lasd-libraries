f1b444c789993242688ceb9274803d721390fdee
sorcho <mariobrosscot@yahoo.it>
2024-05-06 12:05:02 +0200

    completed binarytreelnk

--- scotellaro-mattia-esercizio1/binarytree/lnk/binarytreelnk.cpp
+++ scotellaro-mattia-esercizio1/binarytree/lnk/binarytreelnk.cpp
@@ -3,7 +3,108 @@ namespace lasd {
 
 /* ************************************************************************** */
 
-// ...
+    // ------------ NODE LNK -----------------
+
+    template <typename Data>
+    BinaryTreeLnk<Data>::NodeLnk::NodeLnk(const NodeLnk& nl) {
+        elem = nl.elem;
+        LChild = (nl.LChild != nullptr) ? new NodeLnk(*nl.LChild) : nullptr;
+        RChild = (nl.RChild != nullptr) ? new NodeLnk(*nl.RChild) : nullptr;
+    }
+
+    template <typename Data>
+    BinaryTreeLnk<Data>::NodeLnk::NodeLnk(NodeLnk&& nl) noexcept {
+        std::swap(elem, nl->elem);
+        std::swap(LChild, nl->LChild);
+        std::swap(RChild, nl->RChild);
+    }
+
+    template <typename Data>
+    inline BinaryTreeLnk<Data>::NodeLnk::~NodeLnk() {
+        if (LChild != nullptr)
+        {
+            delete LChild;
+            LChild = nullptr;
+        }
+        if (RChild != nullptr)
+        {
+            delete RChild;
+            RChild = nullptr;
+        }
+    }
+
+    template <typename Data>
+    typename BinaryTreeLnk<Data>::NodeLnk &BinaryTreeLnk<Data>::NodeLnk::operator=(const NodeLnk& nl) {
+        elem(nl->elem);
+        return *this;
+    }
+
+    template <typename Data>
+    typename BinaryTreeLnk<Data>::NodeLnk &BinaryTreeLnk<Data>::NodeLnk::operator=(NodeLnk&& nl) noexcept {
+        std::swap(elem, nl->elem);
+        return *this;
+    }
+
+    // ----------- BINARY TREE LNK ------------------
+
+    template <typename Data>
+    BinaryTreeLnk<Data>::BinaryTreeLnk(const TraversableContainer<Data>& cont) {
+        size = cont.Size();
+        QueueVec<NodeLnk **> queue;
+        queue.Enqueue(&root);
+        cont.Traverse(
+            [&queue](const Data &data) {
+                NodeLnk *&current = *queue.HeadNDequeue();
+                current = new NodeLnk(data);
+                queue.Enqueue(&current->LChild);
+                queue.Enqueue(&current->RChild);
+            });
+    }
+
+    template <typename Data>
+    BinaryTreeLnk<Data>::BinaryTreeLnk(MappableContainer<Data>&& cont) {
+        size = cont.Size();
+        QueueVec<NodeLnk **> queue;
+        queue.Enqueue(&root);
+        cont.Map(
+            [&queue](Data &data) {
+                NodeLnk *&current = *queue.HeadNDequeue();
+                current = new NodeLnk(std::move(data));
+                queue.Enqueue(&current->LChild);
+                queue.Enqueue(&current->RChild);
+            });
+    }
+
+    template <typename Data>
+    BinaryTreeLnk<Data>::BinaryTreeLnk(const BinaryTreeLnk& bt) {
+        if(bt.root!=nullptr) {
+            root = new NodeLnk(*bt.root);
+            size = bt.Size();
+        }     
+    }
+
+    template <typename Data>
+    BinaryTreeLnk<Data>::BinaryTreeLnk(BinaryTreeLnk&& bt) noexcept {
+        std::swap(root, bt.root);
+        std::swap(size, bt.size);
+    }
+
+    template <typename Data>
+    BinaryTreeLnk<Data> &BinaryTreeLnk<Data>::operator=(const BinaryTreeLnk& bt) {
+        Clear();
+        if(bt.root!=nullptr) {
+            root = new NodeLnk(*bt.root);
+            size = bt.Size();
+        }    
+        return *this;
+    }
+
+    template <typename Data>
+    BinaryTreeLnk<Data> &BinaryTreeLnk<Data>::operator=(BinaryTreeLnk&& bt) noexcept {
+        std::swap(root, bt.root);
+        std::swap(size, bt.size);
+        return *this;
+    }
 
 /* ************************************************************************** */
 
--- scotellaro-mattia-esercizio1/binarytree/lnk/binarytreelnk.hpp
+++ scotellaro-mattia-esercizio1/binarytree/lnk/binarytreelnk.hpp
@@ -13,7 +13,7 @@ namespace lasd {
 /* ************************************************************************** */
 
 template <typename Data>
-class BinaryTreeLnk {
+class BinaryTreeLnk : public virtual MutableBinaryTree<Data>{
   // Must extend MutableBinaryTree<Data>
 
 private:
@@ -22,81 +22,173 @@ private:
 
 protected:
 
-  // using BinaryTree<Data>::???;
+  using BinaryTree<Data>::size;
 
-  // ...
+  struct NodeLnk : virtual MutableBinaryTree<Data>::MutableNode{ // Must extend MutableNode
 
-  struct NodeLnk { // Must extend MutableNode
+  friend class BinaryTreeLnk<Data>;
 
   private:
 
-    // ...
-
   protected:
-
-    // ...
 
   public:
 
-    // ...
+    Data elem {};
+    NodeLnk* LChild = nullptr;
+    NodeLnk* RChild = nullptr;
+
+    NodeLnk() = default;
+
+    NodeLnk(const Data& data) : elem(data) {};
+    NodeLnk(Data&& data) noexcept : elem(std::move(data)) {};
+
+    //Copy Constructor
+    NodeLnk(const NodeLnk&);
+
+    //Move Constructor
+    NodeLnk(NodeLnk&&) noexcept;
+
+    virtual ~NodeLnk();
+
+    //Copy Assignament
+    NodeLnk& operator=(const NodeLnk&);
+
+    //Move Assignament
+    NodeLnk& operator=(NodeLnk&&) noexcept;
+
+    // Specific member functions
+
+    virtual bool inline IsLeaf() const noexcept override { 
+      return (!(HasLeftChild() || HasRightChild())); 
+    };
+
+    virtual inline Data& Element() noexcept override {
+      return elem;
+    };
+
+    virtual const inline Data& Element() const noexcept override {
+      return elem;
+    };
+
+    virtual bool inline HasLeftChild() const noexcept override{
+      return (LChild == nullptr ? false : true);
+    };
+
+    virtual bool inline HasRightChild() const noexcept override{
+      return (RChild == nullptr ? false : true);
+    };
+
+    virtual inline NodeLnk& LeftChild() override {
+      if(IsLeaf()) {
+        throw std::out_of_range("Errore: figlio sinistro inesistente");
+      }
+      return *LChild;
+    }; 
+
+    virtual const inline NodeLnk& LeftChild() const override {
+      if(IsLeaf()) {
+        throw std::out_of_range("Errore: figlio sinistro inesistente");
+      }
+      return *LChild;
+    }; 
+
+    virtual inline NodeLnk& RightChild() override {
+      if(IsLeaf()) {
+        throw std::out_of_range("Errore: figlio destro inesistente.");
+      }
+      return *RChild;
+    };
+
+    virtual const inline NodeLnk& RightChild() const override {
+      if(IsLeaf()) {
+        throw std::out_of_range("Errore: figlio destro inesistente.");
+      }
+      return *RChild;
+    };
 
   };
+
+  NodeLnk* root = nullptr;
 
 public:
 
   // Default constructor
-  // BinaryTreeLnk() specifiers;
+  BinaryTreeLnk() = default;
 
   /* ************************************************************************ */
 
   // Specific constructors
-  // BinaryTreeLnk(argument) specifiers; // A binary tree obtained from a TraversableContainer
-  // BinaryTreeLnk(argument) specifiers; // A binary tree obtained from a MappableContainer
+  BinaryTreeLnk(const TraversableContainer<Data>&); // A binary tree obtained from a TraversableContainer
+  BinaryTreeLnk(MappableContainer<Data>&&); // A binary tree obtained from a MappableContainer
 
   /* ************************************************************************ */
 
   // Copy constructor
-  // BinaryTreeLnk(argument) specifiers;
+  BinaryTreeLnk(const BinaryTreeLnk&);
 
   // Move constructor
-  // BinaryTreeLnk(argument) specifiers;
+  BinaryTreeLnk(BinaryTreeLnk&&) noexcept;
 
   /* ************************************************************************ */
 
   // Destructor
-  // ~BinaryTreeLnk() specifiers;
+  virtual ~BinaryTreeLnk() {
+    delete root;
+  };
 
   /* ************************************************************************ */
 
   // Copy assignment
-  // type operator=(argument) specifiers;
+  BinaryTreeLnk<Data>& operator=(const BinaryTreeLnk&);
 
   // Move assignment
-  // type operator=(argument) specifiers;
-
+  BinaryTreeLnk<Data>& operator=(BinaryTreeLnk&&) noexcept;
   /* ************************************************************************ */
 
   // Comparison operators
-  // type operator==(argument) specifiers;
-  // type operator!=(argument) specifiers;
+  inline bool operator==(const BinaryTreeLnk& btl) const noexcept { 
+    return BinaryTree<Data>::operator==(btl); 
+  };
+
+  inline bool operator!=(const BinaryTreeLnk& btl) const noexcept { 
+    return BinaryTree<Data>::operator!=(btl); 
+  };
+
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from BinaryTree)
 
-  // type Root() specifiers; // Override BinaryTree member (throw std::length_error when empty)
+  virtual inline const NodeLnk& Root() const override {
+    if(this->Empty()) {
+      throw std::length_error("Errore: albero vuoto.");
+    }
+    return *root;
+  }; // Override BinaryTree member (throw std::length_error when empty)
 
   /* ************************************************************************ */
 
   // Specific member function (inherited from MutableBinaryTree)
 
-  // type Root() specifiers; // Override MutableBinaryTree member (throw std::length_error when empty)
+  virtual inline NodeLnk& Root() override {
+    if(this->Empty()) {
+      throw std::length_error("Errore: albero vuoto.");
+    }
+    return *root;
+  }; // Override MutableBinaryTree member (throw std::length_error when empty)
 
   /* ************************************************************************ */
 
   // Specific member function (inherited from ClearableContainer)
 
-  // type Clear() specifiers; // Override ClearableContainer member
+  virtual inline void Clear() override { 
+    if (root != nullptr) { 
+      delete root; 
+      root=nullptr; 
+    } 
+    size = 0; 
+  }; // Override ClearableContainer member
 
 };
 
